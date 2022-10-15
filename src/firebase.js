9418d4d5ed50bd39886466fd1cad3716562e794f
eternalcoderss <shivamchoudhary602@Dmail.com>
2022-10-15 16:16:02 +0530

    firebase file update today

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,14 @@
 import firebase from "firebase";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
-  authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
-  projectId: "disneyplus-clone-a33d5",
-  storageBucket: "disneyplus-clone-a33d5.appspot.com",
-  messagingSenderId: "37918794208",
-  appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",
-  measurementId: "G-DRVLJKWRWG",
+  apiKey: "AIzaSyDsG2LS0o8j1RrWeTko5YKyvDVMfVnZBp8",
+  authDomain: "cp-disney-plus-clone-final.firebaseapp.com",
+  projectId: "cp-disney-plus-clone-final",
+  storageBucket: "cp-disney-plus-clone-final.appspot.com",
+  messagingSenderId: "794648566736",
+  appId: "1:794648566736:web:5e29e2d00b8cd60a6937d5",
+  measurementId: "G-6GXVQGV33E"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
